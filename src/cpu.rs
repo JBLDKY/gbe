cc5dcf7d379682925a0f8d5428845e054776d2dd
JBLDSKY <JBLDSKY@gmail.com>
2024-01-28 23:17:31 +0100

    implementation of the (basic) ADD instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,10 +1,13 @@
 use crate::cpu::Instruction::ADD;
 use crate::registers::Registers;
 
+#[allow(dead_code)]
 enum Instruction {
     ADD(ArithmeticTarget),
 }
 
+#[derive(Copy, Clone)]
+#[allow(dead_code)]
 enum ArithmeticTarget {
     A,
     B,
@@ -23,6 +26,7 @@ pub struct CPU {
     registers: Registers,
 }
 
+#[allow(dead_code)]
 impl CPU {
     pub fn new() -> CPU {
         CPU {
@@ -36,27 +40,36 @@ impl CPU {
         };
     }
 
-    fn add(&mut self, target: ArithmeticTarget) -> u8 {
-        let (new, overflow) = match target {
-            ArithmeticTarget::A => self.registers.a.overflowing_add(self.registers.a),
-            ArithmeticTarget::B => self.registers.b.overflowing_add(self.registers.b),
-            ArithmeticTarget::C => self.registers.c.overflowing_add(self.registers.c),
-            ArithmeticTarget::D => self.registers.d.overflowing_add(self.registers.d),
-            ArithmeticTarget::E => self.registers.e.overflowing_add(self.registers.e),
-            ArithmeticTarget::F => {
-                Into::<u8>::into(self.registers.f).overflowing_add(self.registers.f.into())
-            }
-            ArithmeticTarget::H => self.registers.h.overflowing_add(self.registers.h),
-            ArithmeticTarget::L => self.registers.l.overflowing_add(self.registers.l),
-            _ => unimplemented!(),
-            // ArithmeticTarget::HL => self.registers.HL.overflowing_add(self.registers.hl),
-            // ArithmeticTarget::D8 => self.registers.D8.overflowing_add(self.registers.d8),
-        };
-        self.registers.f.zero = overflow;
-        new
+    #[inline(always)]
+    fn add(&mut self, target: ArithmeticTarget) {
+        // read a value from the register
+        let value = self.read_register(&target);
+
+        let (new_value, overflow) = self.registers.a.overflowing_add(value);
+
+        // Remember to set the flags
+        // TODO: Figure out what half_carry actually means
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.carry = overflow;
+        self.registers.f.subtract = false;
+
+        // The half-carry flag is checked during addition to see if there's a carry from the 4th bit to the 5th bit.
+        // It is used for BCD (Binary Coded Decimal) operations. The half-carry is set if the addition
+        // of the lower nibbles (4 bits) of the accumulator and the value is greater than 0xF, indicating
+        // an overflow from the lower nibble to the upper nibble. We use `0x10` to check the 5th bit because
+        // if this bit is set, it indicates the lower nibble overflowed after addition.
+        // Example: If the lower nibble of `a` is `0b1111` (0xF) and we add `0b0001` (1) to it,
+        // the result is `0b1 0000` (0x10), which sets the 5th bit, indicating a half-carry.
+        self.registers.f.half_carry = ((self.registers.a & 0xf) + (value & 0xf)) & 0x10 == 0x10;
+
+        // Update register `a`
+        // I guess this function will only handle adding for register `a`
+        // SP, DE, R8, BC might need to be handled elsewhere
+        self.registers.a = new_value;
     }
 
-    fn read_register(&self, target: ArithmeticTarget) -> u8 {
+    #[inline(always)]
+    fn read_register(&self, target: &ArithmeticTarget) -> u8 {
         match target {
             ArithmeticTarget::A => self.registers.a,
             ArithmeticTarget::B => self.registers.b,
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
-mod registers;
-mod flags_registers;
 mod cpu;
+mod flags_registers;
+mod registers;
 
 fn main() {
-}
+    let a = 0b1111;
+    let value = 0b1111;
+    dbg!(0b10010 & 0x10);
+    ((a & 0xf) + (value & 0xf)) & 0x10
 
+    // Checking half-carry
+    // ((a & 0xf) + (value & 0xf)) & 0x10
+    //
+    // All of this is just to unset the first 4 bits {
+    // Compares the last 4 bits of the current value in the `a` register with 1111
+    // It will essentially return a new byte where the last 4 bits are only set
+    // if they were also set on register `a`.
+    // (a & 0xf)
+    //
+    // Idem for the new value.
+    // (value & 0xf)
+    //
+    // The next step is to add them together
+    // (a & 0xf) + (value & 0xf)
+    // }
+    //
+    // Now we just have the sum of the last 4 bits of the two values.
+    //
+    // Logically, all we need to do now, if the 5th bit is set on the result. This is simply
+    // done by comparing to 0x10. We don't need to worry about the 5th bit not being set
+    // because it carries over to the 6th bit as we kept only the last 4 bits of `a` and the value
+    // we're adding.
+    // e.g.: 0b1111 + 0b1111 = 0b0001 1110 // in the biggest possible addition, we still dont set
+    // bit #6
+}
