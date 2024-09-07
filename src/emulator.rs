fbc8f1e09973b141004103b1dc2ba05a97eb4883
JBLDSKY <JBLDSKY@gmail.com>
2024-09-07 15:08:15 +0200

    Added SDL to main loop

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,5 +1,80 @@
 use crate::{cpu::CPU, gpu::GPU, mem::MemCtx};
+use sdl2::keyboard::Keycode;
+use sdl2::video::Window;
+use sdl2::{event::Event, render::Canvas};
+use sdl2::{EventPump, Sdl};
 use std::time::{Duration, Instant};
+
+const TILEMAP_HEIGHT: usize = 32;
+const TILEMAP_WIDTH: usize = 32;
+const TILE_SIZE: u32 = 8;
+const SCREEN_WIDTH: u32 = 160;
+const SCREEN_HEIGHT: u32 = 144;
+const SCALE: u32 = 3;
+
+enum EventResult {
+    Continue,
+    Stop,
+}
+pub struct Screen {
+    canvas: Canvas<Window>,
+    event_pump: EventPump,
+    ctx: Sdl,
+}
+
+impl Screen {
+    fn new() -> Self {
+        let ctx = sdl2::init().unwrap();
+        let video_subsystem = ctx.video().unwrap();
+
+        let window = video_subsystem
+            .window(
+                "rust-sdl2 demo: Video",
+                SCREEN_WIDTH * SCALE,
+                SCREEN_HEIGHT * SCALE,
+            )
+            .position_centered()
+            .opengl()
+            .build()
+            .map_err(|e| e.to_string())
+            .unwrap();
+
+        let canvas = window
+            .into_canvas()
+            .build()
+            .map_err(|e| e.to_string())
+            .unwrap();
+
+        let event_pump = ctx.event_pump().unwrap();
+
+        Self {
+            canvas,
+            event_pump,
+            ctx,
+        }
+    }
+
+    fn handle_event(&mut self) -> EventResult {
+        for event in self.event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => return EventResult::Stop,
+                _ => {}
+            }
+        }
+
+        EventResult::Continue
+    }
+
+    fn step(&mut self) {
+        self.canvas.clear();
+        self.canvas.present();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+    }
+}
 
 pub struct Emulator<T: MemCtx> {
     pub cpu: CPU,
@@ -26,7 +101,9 @@ impl<T: MemCtx> Emulator<T> {
         let mut cycles_this_frame = 0;
         let mut last_update = Instant::now();
 
-        loop {
+        let mut screen = Screen::new();
+
+        'running: loop {
             let cycles = self.cpu.step(&mut self.mem) as usize;
 
             cycles_this_frame += cycles;
@@ -46,6 +123,12 @@ impl<T: MemCtx> Emulator<T> {
                 }
 
                 last_update = now;
+            }
+
+            screen.step();
+            match screen.handle_event() {
+                EventResult::Continue => (),
+                EventResult::Stop => break 'running,
             }
         }
     }
--- src/gpu.rs
+++ src/gpu.rs
@@ -1,11 +1,5 @@
-use log::debug;
-
 use crate::mem::MemCtx;
-
-const OAM_CYCLES: u16 = 80;
-const VRAM_XFER_CYCLES: u16 = 172;
-const HBLANK_CYCLES: u16 = 200;
-const VBLANK_CYCLES: u16 = 456;
+use log::debug;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Mode {
@@ -17,15 +11,7 @@ pub enum Mode {
 
 #[derive(Debug)]
 pub struct GPU {
-    vblank_interrupt_enabled: bool,
-    hblank_interrupt_enabled: bool,
     oam_interrupt_enabled: bool,
-    lyc_interrupt_enabled: bool,
-    scroll_x: u8,
-    scroll_y: u8,
-    bg_palette: u8,
-    obj_palette0: u8,
-    obj_palette1: u8,
     ly: u8,
     line_y_compare: u8,
     mode: Mode,
@@ -37,15 +23,7 @@ pub struct GPU {
 impl GPU {
     pub fn new() -> Self {
         GPU {
-            vblank_interrupt_enabled: false,
-            hblank_interrupt_enabled: false,
             oam_interrupt_enabled: false,
-            lyc_interrupt_enabled: false,
-            scroll_x: 0,
-            scroll_y: 0,
-            bg_palette: 0,
-            obj_palette0: 0,
-            obj_palette1: 0,
             ly: 0,
             line_y_compare: 0,
             mode: Mode::OAMSearch,
@@ -98,53 +76,53 @@ impl GPU {
             }
         };
     }
-
-    fn handle_hblank<T: MemCtx>(&mut self, mem: &mut T) {
-        self.cycles %= HBLANK_CYCLES;
-        self.increment_line_y(mem);
-        self.mode = if self.ly >= 144 {
-            Mode::VBlank
-        } else {
-            Mode::OAMSearch
-        };
-    }
-
-    fn handle_vblank<T: MemCtx>(&mut self, mem: &mut T) {
-        self.cycles %= VBLANK_CYCLES;
-        self.increment_line_y(mem);
-        if self.ly > 153 {
-            self.ly = 0;
-            self.mode = Mode::OAMSearch;
-            if self.oam_interrupt_enabled {
-                // TODO: something with setting a VBLank interrupt
-            }
-        }
-        mem.write(0xFF0F, mem.read(0xFF0F) | 0x01);
-    }
-
-    fn handle_oam_search(&mut self) {
-        self.cycles %= OAM_CYCLES;
-        self.mode = Mode::Drawing;
-    }
-
-    fn handle_vram_transfer(&mut self) {
-        self.cycles %= VRAM_XFER_CYCLES;
-        self.mode = Mode::HBlank;
-    }
-
-    fn enter_vblank_mode<T: MemCtx>(&mut self, mem: &mut T) {
-        self.mode = Mode::VBlank;
-        // The following toggles on or off
-        mem.write(0xFF0F, mem.read(0xFF0F) | 0x01);
-    }
-
-    fn increment_line_y<T: MemCtx>(&mut self, mem: &mut T) {
-        self.ly = self.ly.wrapping_add(1);
-        mem.write(0xFF44, self.ly);
-        if self.ly == 144 {
-            self.enter_vblank_mode(mem);
-        } else if self.ly > 153 {
-            self.ly = 0;
-        }
-    }
 }
+
+// fn handle_hblank<T: MemCtx>(&mut self, mem: &mut T) {
+//     self.cycles %= HBLANK_CYCLES;
+//     self.increment_line_y(mem);
+//     self.mode = if self.ly >= 144 {
+//         Mode::VBlank
+//     } else {
+//         Mode::OAMSearch
+//     };
+// }
+//
+// fn handle_vblank<T: MemCtx>(&mut self, mem: &mut T) {
+//     self.cycles %= VBLANK_CYCLES;
+//     self.increment_line_y(mem);
+//     if self.ly > 153 {
+//         self.ly = 0;
+//         self.mode = Mode::OAMSearch;
+//         if self.oam_interrupt_enabled {
+//             // TODO: something with setting a VBLank interrupt
+//         }
+//     }
+//     mem.write(0xFF0F, mem.read(0xFF0F) | 0x01);
+// }
+//
+// fn handle_oam_search(&mut self) {
+//     self.cycles %= OAM_CYCLES;
+//     self.mode = Mode::Drawing;
+// }
+//
+// fn handle_vram_transfer(&mut self) {
+//     self.cycles %= VRAM_XFER_CYCLES;
+//     self.mode = Mode::HBlank;
+// }
+//
+// fn enter_vblank_mode<T: MemCtx>(&mut self, mem: &mut T) {
+//     self.mode = Mode::VBlank;
+//     // The following toggles on or off
+//     mem.write(0xFF0F, mem.read(0xFF0F) | 0x01);
+// }
+//
+// fn increment_line_y<T: MemCtx>(&mut self, mem: &mut T) {
+//     self.ly = self.ly.wrapping_add(1);
+//     mem.write(0xFF44, self.ly);
+//     if self.ly == 144 {
+//         self.enter_vblank_mode(mem);
+//     } else if self.ly > 153 {
+//         self.ly = 0;
+//     }
+// }
